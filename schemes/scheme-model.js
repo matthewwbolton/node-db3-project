d7be060460f700ac35cf9b2bbec4e2558b7a4801
Matthew Bolton <matthewwbolton@gmail.com>
2020-04-15 18:56:37 -0400

    completed all endpoint testing with DB helper functions

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -9,18 +9,62 @@ module.exports = {
   add,
   update,
   remove,
+  addStep,
 };
 
 function find() {
   return db("schemes");
 }
 
-function findById(id) {}
+function findById(id) {
+  return db("schemes").where({ id }).first();
+}
 
-function findSteps(id) {}
+function findSteps(id) {
+  //   return db("schemes as s")
+  //     .join("steps as st", "s.id", "st.scheme_id")
+  //     .select(
+  //       "s.id as Scheme ID",
+  //       "s.scheme_name as Scheme Name",
+  //       "st.step_number as Step Number",
+  //       "st.instructions as Instructions"
+  //     )
+  //     .where({ id });
 
-function add(scheme) {}
+  return db
+    .select(
+      "s.id as ID",
+      "s.scheme_name as Scheme Name",
+      "st.step_number as Step Number",
+      "st.step_number as Step Number",
+      "st.instructions as Instructions"
+    )
+    .from("schemes as s")
+    .join("steps as st", "s.id", "st.scheme_id")
+    .where("s.id", id);
+}
 
-function update(changes, id) {}
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme, "id")
+    .then(([id]) => {
+      return findById(id);
+    });
+}
 
-function remove(id) {}
+function update(changes, id) {
+  return db("schemes")
+    .update(changes)
+    .where({ id })
+    .then(() => {
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  return db("schemes").where({ id }).del();
+}
+
+function addStep(step, scheme_id) {
+  return db("steps").insert(step).where({ id: scheme_id });
+}
